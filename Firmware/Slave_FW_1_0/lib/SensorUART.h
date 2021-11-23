97871029cc69e633f25f725bea8fee0fa20a1b1b
Yeray Candel <ycansam@epsg.upv.es>
2021-11-23 20:05:51 +0100

    getType y getMeasure Implementados
    Añadidos testUart, y se han testeado 5 UARTS

--- Firmware/Slave_FW_1_0/lib/SensorUART.h
+++ Firmware/Slave_FW_1_0/lib/SensorUART.h
@@ -21,6 +21,17 @@ private:
     uint16_t baudios = 9600;
     SoftwareSerial *sensorSerial;
 
+    /**
+     * Obtiene la concentracion del gas
+     * @param highConcentration concentracion alta del gas
+     * @param lowConcentration concentracion baja del gas
+     * @returns valor de la concentracion del gas
+     */
+    int getGasConcentration(byte highConcentration, byte lowConcentration)
+    {
+        return highConcentration * 256 + lowConcentration;
+    }
+    
 public:
     /**
      * Constructor de la clase SensorUART
@@ -31,55 +42,92 @@ public:
         (*this).TX = tx;
         sensorSerial = new SoftwareSerial(rx, tx);
     }
-    /**
-     * Inicializa el esclavo por I2C
-     * @param baudios velocidad del Serial
-     */
-    void initSensor(int baudios = 9600)
-    {
-        (*this).baudios = baudios;
-        sensorSerial->begin(baudios);
-        sensorSerial->print("Inicializando");
-        if (sensorSerial->available())
-        {
-            char inByte = sensorSerial->read();
-    
-            Serial.write(inByte);
-        }
-    }
 
-    /**
-     * Obtiene la medida del sensor
-     */
-    void getMeasure(char* c)
+    byte testUart(char *c)
     {
         sensorSerial->begin((*this).baudios);
         delay(500);
         sensorSerial->println(c);
+        byte b;
         if (sensorSerial->available())
         {
-            Serial.write(sensorSerial->read());
+            b = sensorSerial->read();
+            Serial.write(b);
         }
         sensorSerial->end();
         Serial.println();
+        return b;
     }
-    
 
     /**
-     * Obtiene el tipo de sensor
+     * Obtiene la medida del sensor
+     * @returns devuelve un array
      */
-    byte getType()
+    int getMeasure()
+    {
+        Serial.println("Enviando comando : 0xFF 0x01 0x86 0x00 0x00 0x00 0x00 0x00 0x79");
+        byte arrayCommand[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
+
+        // enviando el comando a la uart del sensor
+        for (int i = 0; i < 9; i++)
+        {
+            sensorSerial->write(arrayCommand[i]);
+        }
+
+        Serial.println("Leyendo Sensor..");
+        uint8_t index = 0;
+        byte arrayData[8];
+        // leyendo la respuesta del sensor
+        while (sensorSerial->available() > 0)
+        {
+            // recibiendo la respuesta del tipo
+            Serial.print("Byte: " + index);
+            arrayData[index] = sensorSerial->read();
+            Serial.println(" : " + arrayData[index]);
+
+            Serial.write(sensorSerial->read());
+            index++;
+        }
+
+        // devolviendo la lectura de hexadecimal a decimal
+        return getGasConcentration(arrayData[6], arrayData[7]);
+    }
+
+    /**
+     * Obtiene un array de datos con la informacion del modulo
+     * Informacion de 8 bytes.
+     * 
+     *     0    1    2    3    4    5    6    7    8
+     * EJ: 0x17 0x00 0XC8 0x02 0x00 0x00 0x00 0x01 0x1E
+     */
+    byte *getType()
     {
         sensorSerial->begin((*this).baudios);
         delay(500);
-        sensorSerial->println("5");
-        if (sensorSerial->available())
+
+        // envia el comando de pedir el tipoS
+        Serial.println("Enviando el comando 0xD1");
+        sensorSerial->write(0xD1);
+        Serial.println("Obteniendo datos...");
+        Serial.println();
+
+        // index para el array
+        uint8_t index = 0;
+        byte arrayData[8];
+        while (sensorSerial->available() > 0)
         {
+            Serial.print("Byte: " + index);
+            // recibiendo la respuesta del tipo
+            arrayData[index] = sensorSerial->read();
+            Serial.println(" : " + arrayData[index]);
             Serial.write(sensorSerial->read());
+            index++;
         }
         sensorSerial->end();
         Serial.println();
-        return 0x25;
+        Serial.println("Tipo recibido.");
+
+        return arrayData;
     }
 };
 
