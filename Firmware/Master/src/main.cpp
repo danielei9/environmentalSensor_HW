318b60561de474acdcf96e7bd1089995bf84479e
danielei9 <dbs99@hotmail.com>
2021-12-19 19:49:32 +0100

    upds

--- Firmware/Master/src/main.cpp
+++ Firmware/Master/src/main.cpp
@@ -47,15 +47,7 @@ TinyGsm modem(SerialAT);
 
 #define CFG_sx1276_radio 1 // HPD13A LoRa SoC
 
-// Pins for LORA chip SPI interface, reset line and interrupt lines
-#define LORA_SCK  (18)
-#define LORA_CS   (5)
-#define LORA_MISO (19)
-#define LORA_MOSI (23)
-#define LORA_RST  (14) // old 15
-#define LORA_IRQ  (2) // old 27
-#define LORA_IO1  (32) // old 33
-#define LORA_IO2  NOT_A_PIN
+
 
 #endif
 TinyGsmClientSecure client(modem);
@@ -130,7 +122,6 @@ void setup()
 {
   Serial.begin(115200);
   // gps.init();
-  initModbus();
 
   publisher->initPublisher();
 
